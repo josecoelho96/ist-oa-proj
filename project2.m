ab9baa1f2830183487218f3e4a47d698a2450951
Jose Coelho <josecoelho96@gmail.com>
2017-12-07 01:37:43 +0000

    Change project files hierarchy

---
+++ project2.m
@@ -0,0 +1,10 @@
+% Optimization and Algorithms
+% Project 2
+% 2017/2018 Fall Semester
+% Group 23
+% José Coelho, 81013
+% Miguel de Moura, 78887
+% Gonçalo Pereira, 81602
+
+% Part1 : simulations without noise
+part1a
