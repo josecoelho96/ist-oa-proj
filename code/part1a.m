a2507697c9a2e2ddba048eacef99a3a098b088ab
Jose Coelho <josecoelho96@gmail.com>
2017-12-07 02:55:23 +0000

    Change code to support parallel computing

--- code/part1a.m
+++ code/part1a.m
@@ -26,8 +26,6 @@ methods = 4; % Methods being studied ( LS, l1, P1, P2(1) )
 
 % save results
 results = zeros(methods, length(reliable_sensors_list));
-%preallocations
-bi = zeros(m, 1, k);
 
 fprintf('Realizing %d Monte Carlo simulations without noise. ', MCexperiments);
 toc;
@@ -35,11 +33,15 @@ toc;
 for s_index = 1:length(reliable_sensors_list)
     
     s = reliable_sensors_list(s_index);
+    reliable_sensors = [ones(1, s) zeros(1, k-s)];
     
     fprintf('Considered %d reliable sensors. ', s);
     toc;
     
-    for j=1:MCexperiments
+    parfor j=1:MCexperiments
+        
+        %preallocations
+        bi = zeros(m, 1, k);
         
         % unknown vector is modeled as x0 ~ N(0, n^(-1/2)In)
         x0 = mvnrnd(zeros(1, n), n^(-0.5)*eye(n))';
@@ -62,30 +64,33 @@ for s_index = 1:length(reliable_sensors_list)
         C = permute(Ai, [1 3 2]);
         A = reshape(C, [], size(Ai, 2), 1);
         
-        reliable_sensors = [ones(1, s) zeros(1, k-s)];
-        
         % LS method
         x_ls = ls_method(A, b, n);
-        results_ls = sensor_validation(Ai, bi, x_ls, threshold, k, s);
-        results(1, s_index) = results(1, s_index) + isequal(reliable_sensors, results_ls);
+        sensor_results_ls = sensor_validation(Ai, bi, x_ls, threshold, k, s);
+        results_ls(j, s_index) = isequal(reliable_sensors, sensor_results_ls);
+        
         
         % l1 method
         x_l1 = l1_method(A, b, n);
-        results_l1 = sensor_validation(Ai, bi, x_l1, threshold, k, s);
-        results(2, s_index) = results(2, s_index) + isequal(reliable_sensors, results_l1);
+        sensor_results_l1 = sensor_validation(Ai, bi, x_l1, threshold, k, s);
+        results_l1(j, s_index) = isequal(reliable_sensors, sensor_results_l1);
         
         % P1 method
         x_p1 = p1_method(Ai, bi, n, k);
-        results_p1 = sensor_validation(Ai, bi, x_p1, threshold, k, s);
-        results(3, s_index) = results(3, s_index) + isequal(reliable_sensors, results_p1);
+        sensor_results_p1 = sensor_validation(Ai, bi, x_p1, threshold, k, s);
+        results_p1(j, s_index) = isequal(reliable_sensors, sensor_results_p1);
         
         % P2(1) method
         x_p2_1 = p2_1_method(Ai, bi, n, k, x_p1, delta);
-        results_p2_1 = sensor_validation(Ai, bi, x_p2_1, threshold, k, s);
-        results(4, s_index) = results(4, s_index) + isequal(reliable_sensors, results_p2_1);
+        sensor_results_p2_1 = sensor_validation(Ai, bi, x_p2_1, threshold, k, s);
+        results_p2_1(j, s_index) = isequal(reliable_sensors, sensor_results_p2_1);
     end
 end
 
+results(1,:) = sum(results_ls, 1);
+results(2,:) = sum(results_l1, 1);
+results(3,:) = sum(results_p1, 1);
+results(4,:) = sum(results_p2_1, 1);
 results = (results./MCexperiments).*100;
 
 % Print results
