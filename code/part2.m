a5a100ebaabd84e77d4c3200570b74230241ceeb
Jose Coelho <josecoelho96@gmail.com>
2017-12-12 20:54:26 +0000

    Add part2 solution

--- code/part2.m
+++ code/part2.m
@@ -1,61 +1,105 @@
 close all;
 clearvars;
 
+save_folder = '../results/part2/';
+save_str = [datestr(now,'dd-mm-yy','local'),'_',datestr(now,'hh-MM-ss','local')];
+diary([save_folder, 'log_', save_str, '.txt'])
+tic;
+
 k = 16; % Number of sensors
 m = 4; % Size of observation vectors b
 n = 20; % Size of unknown vector x
 reliable_sensors_list = [6 8 10 12 14]; % Number of consistent sensors
-restriction_delta = 10^-6;
-threshold_value = 10^-4;
+restriction_delta = 10^-10;
+threshold = 10^-4;
+MCexperiments = 1000;
 
-s = 14;
+fprintf('Realizing %d Monte Carlo simulations without noise.\n', MCexperiments);
+fprintf('Delta: %e.\n', restriction_delta);
+
+
+for s_index = 1:length(reliable_sensors_list)
     
-reliable_sensors = [ones(1, s) zeros(1, k-s)];
+    s = reliable_sensors_list(s_index);
+    
+    fprintf('Considered %d reliable sensors. ', s);
+    toc;
+    
+    reliable_sensors = [ones(1, s) zeros(1, k-s)];
+    
+    parfor j=1:MCexperiments
 
-%preallocations
-bi = zeros(m, 1, k);
+        %preallocations
+        bi = zeros(m, 1, k);
 
-% unknown vector is modeled as x0 ~ N(0, n^(-1/2)In)
-x0 = mvnrnd(zeros(1, n), n^(-1)*eye(n))';
+        % unknown vector is modeled as x0 ~ N(0, n^(-1/2)In)
+        x0 = mvnrnd(zeros(1, n), n^(-1)*eye(n))';
 
-% Entries of matrix A are drawn independently from N(0, 1)
-Ai = randn(m, n, k);
+        % Entries of matrix A are drawn independently from N(0, 1)
+        Ai = randn(m, n, k);
 
-% generate sensor observation data b
-% consistent observations
-for i=1:s
-    bi(:, :, i) = Ai(:, : ,i)*x0;
-end
+        % generate sensor observation data b
+        % consistent observations
+        for i=1:s
+            bi(:, :, i) = Ai(:, : ,i)*x0;
+        end
 
-% unreliable sensors
-for i=s+1:k
-    bi(:, : , i) = mvnrnd(zeros(1, m), eye(m))';
-end
-        
-% CVX problem solving
-cvx_begin quiet
-    variable x(n, k)
-    variable L(n, 1)
-    % define cost function
-    for i=1:k
-        % f(i) = (x(:,i)-L)'*(x(:,i)-L);
-        f(i) = norm(x(:,i)-L);
+        % unreliable sensors
+        for i=s+1:k
+            bi(:, : , i) = mvnrnd(zeros(1, m), eye(m))';
+        end
+
+        % % CVX problem solving
+        % cvx_begin quiet
+        %     variable x(n, k)
+        %     variable L(n, 1)
+        %     % define cost function
+        %     for i=1:k
+        %         % f(i) = (x(:,i)-L)'*(x(:,i)-L);
+        %         f(i) = norm(x(:,i)-L);
+        %     end
+        %     minimize(sum(f))
+        %     subject to
+        %     for i=1:k
+        %         norm(bi(:,:,i) - Ai(:,:,i)*x(:,i)) <= restriction_delta;
+        %         % (bi(:,:,i)-Ai(:,:,i)*x(:,i))'*(bi(:,:,i)-Ai(:,:,i)*x(:,i)) <= restriction_delta;
+        %     end
+        % cvx_end
+
+
+        x0 = randn(n, k) / sqrt(n);
+        L0 = mean(x0, 2);
+
+        lambda_est = matching_solutions( Ai, bi, n, k, restriction_delta, x0, L0);
+
+        % check for reliable sensors
+        method_reliable_sensors = sensor_validation( Ai, bi, lambda_est, threshold, k, s);
+
+        list_results(j, s_index) = isequal(reliable_sensors, method_reliable_sensors);
+
     end
-    minimize(sum(f))
-    subject to
-    for i=1:k
-        norm(bi(:,:,i) - Ai(:,:,i)*x(:,i)) <= restriction_delta;
-        % (bi(:,:,i)-Ai(:,:,i)*x(:,i))'*(bi(:,:,i)-Ai(:,:,i)*x(:,i)) <= restriction_delta;
-    end
-cvx_end
-        
-% check for reliable sensors
-method_reliable_sensors = zeros(1, k);
-for i=1:s
-    %norm inf must be lower than the threshold for all reliable sensors
-    method_reliable_sensors(i) = norm(bi(:,:,i) - Ai(:,:, i)*L, inf) <= threshold_value;
-end
-results = isequal(reliable_sensors, method_reliable_sensors);
 
-disp(reliable_sensors)
-disp(method_reliable_sensors)
+end
+
+results = sum(list_results, 1);
+results = (results/MCexperiments) * 100;
+
+% Print results
+f = figure('Position',[440 500 500 140]);
+% Create the column and row names in cell arrays
+cnames = {'s=6','s=8','s=10', 's=12', 's=14'};
+rnames = {'MS'};
+
+% Create the uitable
+t = uitable(f,'Data',results,...
+            'ColumnName',cnames,...
+            'RowName',rnames);
+
+% Set width and height
+t.Position(3) = t.Extent(3);
+t.Position(4) = t.Extent(4);
+
+print([save_folder, 'table_', save_str], '-dpng');
+save([save_folder, 'workspace_', save_str]);
+toc
+diary off
