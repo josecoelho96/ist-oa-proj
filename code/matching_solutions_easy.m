7b12f2cb45fe1f0444121b890758aad9ee253bbf
Jose Coelho <josecoelho96@gmail.com>
2017-12-13 00:34:09 +0000

    Update matching solutions method without log

--- code/matching_solutions_easy.m
+++ code/matching_solutions_easy.m
@@ -1,16 +1,18 @@
 function [ L ] = matching_solutions_easy( A, b, n, k, delta)
-    cvx_begin quiet
-        variable x(n, k)
-        variable L(n, 1)
-        % define cost function
-        f = 0;
-        for i=1:k
-            f = f + (x(:,i)-L)'*(x(:,i)-L);
-        end
-        minimize(f)
-        subject to
-        for i=1:k
-            (b(:,:,i)-A(:,:,i)*x(:,i))'*(b(:,:,i)-A(:,:,i)*x(:,i)) <= delta;
-        end    
-    cvx_end
+% CVX problem solving
+cvx_begin quiet
+    variable x(n, k)
+    variable L(n, 1)
+    % define cost function
+    for i=1:k
+        % f(i) = (x(:,i)-L)'*(x(:,i)-L);
+        f(i) = norm(x(:,i)-L);
+    end
+    minimize(sum(f))
+    subject to
+    for i=1:k
+        norm(bi(:,:,i) - Ai(:,:,i)*x(:,i)) <= restriction_delta;
+        % (bi(:,:,i)-Ai(:,:,i)*x(:,i))'*(bi(:,:,i)-Ai(:,:,i)*x(:,i)) <= restriction_delta;
+    end
+cvx_end
 end
