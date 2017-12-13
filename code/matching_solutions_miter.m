47bb0ae49b9145bf3621e1931fde11d2804dab39
Jose Coelho <josecoelho96@gmail.com>
2017-12-13 01:57:57 +0000

    Rename MS method for multiple iterations file

--- code/matching_solutions_miter.m
+++ code/matching_solutions_miter.m
@@ -1,4 +1,4 @@
-function [ L ] = matching_solutions_2_iter( A, b, n, k, delta, x0, L0)
+function [ x, L ] = matching_solutions_miter( A, b, n, k, delta, x0, L0)
 
 %     cvx_begin quiet
 %         variable x(n, k) 
@@ -21,7 +21,7 @@ function [ L ] = matching_solutions_2_iter( A, b, n, k, delta, x0, L0)
         % define cost function
         for i=1:k
             % f(i) = ((x(:,i)-L)'*(x(:,i)-L))/((x0(:,i)-L0)'*(x0(:,i)-L0));
-            f(i) =  norm(x(:,i)-L) / norm((x0(:,i)-L0));
+            f(i) =  norm(x(:,i)-L) / norm(x0(:,i)-L0);
         end
 
         minimize(sum(f))
@@ -33,28 +33,4 @@ function [ L ] = matching_solutions_2_iter( A, b, n, k, delta, x0, L0)
             (b(:,:,i)-A(:,:,i)*x(:,i))'*(b(:,:,i)-A(:,:,i)*x(:,i)) <= delta;
         end
     cvx_end
-    
-    x1 = x;
-    L1 = L;
-    
-    cvx_begin quiet
-        variable x(n, k) 
-        variable L(n, 1)
-        % define cost function
-        for i=1:k
-            % f(i) = ((x(:,i)-L)'*(x(:,i)-L))/((x0(:,i)-L0)'*(x0(:,i)-L0));
-            f(i) =  norm(x(:,i)-L) / norm((x1(:,i)-L1));
-        end
-
-        minimize(sum(f))
-
-        subject to
-        for i=1:k
-            % norm(bi(:,:,i) - Ai(:,:,i)*x(:,i)) <= restriction_delta;
-            % norm(bi(:,:,i) - Ai(:,:,i)*x(:,i)) <= sqrt(restriction_delta);
-            (b(:,:,i)-A(:,:,i)*x(:,i))'*(b(:,:,i)-A(:,:,i)*x(:,i)) <= delta;
-        end
-    cvx_end
-    
-    
 end
